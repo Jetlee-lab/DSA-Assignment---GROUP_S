d41e2dfeaea246e1ccdea2a40fa289f60e26b71c
Keith Paul Kato <paulkato460@yahoo.co.uk>
2025-03-14 14:02:34 +0300

    Modified code for Task 1

--- Adjacency_matrix.py
+++ Adjacency_matrix.py
@@ -1,18 +1,46 @@
 
-# Create an adjacency matrix initialized to 0
-# Define the number of cities
-num_cities = 7
+# Define the nodes of the graph
+nodes = ('1', '2', '3', '4', '5', '6', '7')
 
-# Create an adjacency matrix initialized to the given distances
+# edges = (
+#     ('1', '2'), ('1', '3'), ('1', '7'),
+#     ('2', '1'), ('2', '3'), ('2', '4'),
+#     ('3', '1'), ('3', '2'), ('3', '4'), ('3', '5'), ('3', '7'),
+#     ('4', '2'), ('4', '3'), ('4', '5'), ('4', '6'),
+#     ('5', '3'), ('5', '4'), ('5', '6'), ('5', '7'),
+#     ('6', '4'), ('6', '5'), ('6', '7'),
+#     ('7', '1'), ('7', '3'), ('7', '5'), ('7', '6'),
+# )
+
+# Define the edges and corresponding weights of the graph
+edges = (
+    ('1', '2', 12), ('1', '3', 10), ('1', '7', 12),
+    ('2', '3', 8), ('2', '4', 12),
+    ('3', '4', 11), ('3', '5', 3), ('3', '7', 9),
+    ('4', '5', 11), ('4', '6', 10),
+    ('5', '6', 6), ('5', '7', 7),
+    ('6', '7', 9),
+)
+
+# Get the number of nodes
+num_nodes = len(nodes)
+
+# Create an adjacency matrix initialized to 0
 adjacency_matrix = [
-    [0, 12, 10, float('inf'), float('inf'), float('inf'), 12],
-    [12, 0, 8, 12, float('inf'), float('inf'), float('inf')],
-    [10, 8, 0, 11, 3, float('inf'), float('inf')],
-    [float('inf'), 12, 11, 0, 11, float('inf'), float('inf')],
-    [float('inf'), float('inf'), 3, 11, 0, 10, 7],
-    [float('inf'), float('inf'), float('inf'), float('inf'), 10, 0, 9],
-    [12, float('inf'), float('inf'), float('inf'), 7, 9, 0]
+    [0 for i in range(num_nodes)]
+    for j in range(num_nodes)
 ]
+
+for edge in edges:
+    # Get the source and destination node
+    i = nodes.index(edge[0])
+    j = nodes.index(edge[1])
+    weight = edge[2]
+
+    # Add the edge to the adjacency matrix
+    adjacency_matrix[i][j] = weight
+    adjacency_matrix[j][i] = weight
+
 
 # Print the adjacency matrix
 for row in adjacency_matrix:
